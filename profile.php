309e9efad0044005fe792d9c563291de5816b042
KanishaLiyanage <kanishaliyanage97@gmail.com>
2022-07-14 22:52:11 +0530

    user profile updated

--- profile.php
+++ profile.php
@@ -4,19 +4,11 @@
 
 <?php
 
-$p_id = " ";
-$c_id = " ";
-$t_price = " ";
-$o_qty = " ";
-
 if (!isset($_SESSION['cus_id'])) {
     header('Location: landing_page.php');
-}else{
-
 }
 
 ?>
-
 
 <!DOCTYPE html>
 <html lang="en">
@@ -26,13 +18,59 @@ if (!isset($_SESSION['cus_id'])) {
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Profile</title>
-    <link rel="stylesheet" href="css/confirmPurchase.css">
+    <link rel="stylesheet" href="css/profile.css">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
 </head>
 
 <body>
+        <div class="headerBtns">
+        <a class="cartBtn" href="myCart.php"> Cart </a>
+        <a class="favBtn" href="myFavorites.php"> Favorites </a>
+        <a class="ordersBtn" href="myOrders.php"> Purchased Products </a>
+        <a class="editBtn" href="editProfile.php"> Edit Profile </a>
+        </div>
 
-    <h1>My Profile</h1>
+    <center>
+
+        <h1 class="h1">My Profile</h1>
+
+        <?php
+
+        $user_query = "SELECT * FROM customers WHERE customer_id = '{$_SESSION['cus_id']}' LIMIT 1";
+
+        $result = mysqli_query($connection, $user_query);
+
+        if ($result) {
+
+            while ($record = mysqli_fetch_array($result)) {
+
+        ?>
+
+                <div class="profileCard">
+
+                    <p>Image: </p>
+                    <p>ID: <?php echo $record['customer_id']; ?> </p>
+                    <p>User Name: <?php echo $record['username']; ?> </p>
+                    <p>Email: <?php echo $record['email']; ?> </p>
+                    <p>Mobile Number: <?php echo $record['mobile_number']; ?> </p>
+                    <p>Address: <?php echo $record['address']; ?> </p>
+                    <p>Postal Code: <?php echo $record['postal_code']; ?> </p>
+                    <p>City: <?php echo $record['city']; ?> </p>
+                    <p>Province: <?php echo $record['province']; ?> </p>
+                    <p>Country: <?php echo $record['country']; ?> </p>
+
+                </div>
+
+        <?php
+
+            }
+        }
+
+        ?>
+
+        <a class="logoutBtn" href="components/logout.php"> Logout </a>
+
+    </center>
 
 </body>
 
